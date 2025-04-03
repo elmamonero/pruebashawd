bbc822332337ccfee0b37fd29f6f6282d28a266a
elmamonero <debastado0@gmail.com>
2025-04-03 18:09:08 -0400

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -31,42 +31,42 @@ let pp = ''
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 //let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/327f6ad853cb4f405aa80.jpg')
 
-//CREADOR Y OTROS
-global.creador = 'Wa.me/51927238856'
+//creador y otros
+global.creador = 'Wa.me/584262668729'
 global.botreal = `${(conn.user.jid == global.conn.user.jid ? 'Oficial' : 'Sub-Bot')}`
 global.ofcbot = `${conn.user.jid.split('@')[0]}`
-global.asistencia = 'Wa.me/51927238856'
-global.namech = '𝑺𝒉𝒂𝒅𝒐𝒘 𝑩𝒐𝒕 - 𝑴𝑫'
-global.namechannel2 = 'ꨴ ☁꣺ꤪ꤬꤯ꨬꨶ ̷̸̲̼̈́ Mᴏᴏɴ Fᴏʀᴄᴇ Ƭᴇᴀᴍ 彡'
-global.namegrupo = 'Group Shadow Oficial'
-global.namecomu = 'Heavenly Team Community'
-global.namecomu2 = '☕ Heavenly Team Community'
+global.asistencia = 'Wa.me/584262668729'
+global.namech = '˙˒𖥻˚𝐂𝐫𝐨𝐰𝐁𝐨𝐭 - 𝐂𝐡𝐚𝐧𝐧𝐞𝐥﹋꥟'
+global.namechannel2 = 'Pantheon Bot'
+global.namegrupo = 'シ︎ Pantheon-BOT '
+global.namecomu = '『𝗚𝗿𝘂𝗽𝗼 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 PantheonBot✯』'
+global.namecomu2 = 'ThePantheonBot'
 
-//REACCIONES 
+//Reacciones De Comandos.!
 global.rwait = '🕒'
 global.done = '✅'
 global.error = '✖️'
 
-//EMOJIS PREDETERMINADOS
-global.emoji = '🌷'
-global.emoji2 = '🤍'
-global.emoji3 = '☕'
-global.emoji4 = '🍨'
-global.emoji5 = '🍃'
+//Emojis determinado de CrowBot
+global.emoji = '🚩'
+global.emoji2 = '💛'
+global.emoji3 = '🌠'
+global.emoji4 = '🍭'
+global.emoji5 = '🔱'
 global.emojis = [emoji, emoji2, emoji3, emoji4, emoji5].getRandom()
 
-//MENSAJE DE ESPERA 
-global.wait = '*[ ⏳ ] Aguarde un momento...*';
+//mensaje en espera
+global.wait = '🕒 *𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻𝘁𝗼, 𝗦𝗼𝘆 𝗟𝗲𝗻𝘁𝗼 ...*';
 
-//ENLACES
-var grupo = 'https://chat.whatsapp.com/FCS6htvAmlT7nq006lxU4I'  
-var github = 'https://github.com/CrxstianEscobar/ShadowBot-MD' 
-var web = 'https://shadow-bot.vercel.app/' 
-let instagram = 'https://www.instagram.com/shadowbot.md'
+//Enlaces
+var grupo = 'https://chat.whatsapp.com/Fe6gizLeKx83S7plNY0cnC'  
+var github = 'https://whatsapp.com/channel/0029Vb3ahtE96H4Lk7DfEU0B' 
+var web = 'https://whatsapp.com/channel/0029Vb3ahtE96H4Lk7DfEU0B' 
+let instagram = 'https://whatsapp.com/channel/0029Vb3ahtE96H4Lk7DfEU0B'
 
 global.redes = [grupo, github, web, instagram].getRandom()
 
-//IMAGEN
+//Imagen
 let category = "imagen"
 const db = './media/database/db.json'
 const db_ = JSON.parse(fs.readFileSync(db))
@@ -76,80 +76,37 @@ const response = await fetch(randomlink)
 const rimg = await response.buffer()
 global.icons = rimg
 
-//TIEMPO
-var ase = moment().tz('America/Lima'); // Cambia 'America/Lima' por la zona horaria deseada
-var hour = ase.hour(); // Obtiene la hora en la zona horaria elegida
-
-switch(hour) { 
-    case 0: case 1: case 2:
-        hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; 
-        break;
-    case 3: case 4: case 5: case 6:
-        hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; 
-        break;
-    case 7:
-        hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌅'; 
-        break;
-    case 8: case 9:
-        hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; 
-        break;
-    case 10: case 11: case 12: case 13:
-        hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; 
-        break;
-    case 14: case 15: case 16: case 17:
-        hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ xD'; 
-        break;
-    case 18: case 19: case 20: case 21: case 22: case 23:
-        hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; 
-        break;
-}
-
+//• ↳ ◜𝑻𝑰𝑬𝑴𝑷𝑶 𝑹𝑷𝑮◞ • ⚔
+var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 1: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 2: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 3: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 4: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 5: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 6: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 7: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌅'; break; case 8: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 9: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 10: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 11: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 12: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 13: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 14: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 15: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 16: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 17: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 18: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 19: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 20: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 21: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 22: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 23: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break;}
 global.saludo = hour;
 
-// FECHA Y HORA EN FORMATO PERSONALIZADO (ZONA HORARIA PERÚ)
-var fecha = moment().tz('America/Lima');
-var diaSemana = fecha.locale('es').format('dddd'); // Día de la semana en español
-var dia = fecha.format('D'); // Día del mes
-var mes = fecha.locale('es').format('MMMM'); // Mes en español
-var año = fecha.format('YYYY'); // Año
-var hora = fecha.format('h:mm A'); // Hora con AM/PM
-
-// Capitalizar primera letra del día y el mes
-diaSemana = diaSemana.charAt(0).toUpperCase() + diaSemana.slice(1);
-mes = mes.charAt(0).toUpperCase() + mes.slice(1);
-
-global.fechaHora = `${diaSemana}, ${dia} de ${mes} del ${año} │ Hora: ${hora}`;
-
-//TAGS & STICKERS
+//tags
 global.nombre = conn.getName(m.sender)
 global.taguser = '@' + m.sender.split("@s.whatsapp.net")
 var more = String.fromCharCode(8206)
 global.readMore = more.repeat(850)
 
-global.authsticker = `\n\n🍪 mᥲძᥱ ᑲᥡ:\n↳協 ֪ Տһᥲ͡ძ͜᥆፝֟ա  ⚡︎  Uᥣ𝗍rᥲ ׄ🍒˚ .\n\n👤 rᥱ𝗊ᥙіrᥱძ ᑲᥡ\n↳@${conn.getName(m.sender)}`;
+global.authsticker = `┊🤖Bot\n┊↳${botname}\n\n┊👑Usuario\n┊↳ @${conn.getName(m.sender)}`;
 
-global.packsticker = `ꨴ ☁꣺ꤪ꤬꤯ꨬꨶ ̷̸̲̼̈́ Mᴏᴏɴ Fᴏʀᴄᴇ Ƭᴇᴀᴍ 彡\n↳@moonforce_team\n\n☘️ іᥒ𝖿᥆rmᥲ𝗍і᥆ᥒ\n↳Wa.me/51927238856`
+global.packsticker = `Pantheon Bot┊ Wa.me/584262668729`
 
-//FAKES
-global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+//Fakes
+global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=584262668729:584262668729\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363318267632676@newsletter', newsletterName: "˚₊·͟͟͟͟͟͟͞͞͞͞͞͞𝑴𝒐𝒐𝒏 𝑭𝒐𝒓𝒄𝒆 - 𝑻𝒆𝒂𝒎 ೃ࿔₊•", serverMessageId: -1 }
+global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363392742449171@newsletter', newsletterName: "Pantheon Bot", serverMessageId: -1 }
 }}, { quoted: m }
 
-//ID CANALES
-global.idchannel = '120363357231409846@newsletter'
-global.canalIdM = ["120363357231409846@newsletter", "120363357231409846@newsletter", "120363357231409846@newsletter"]
-global.canalNombreM = ["⏤͟͟͞͞𝖲𝗁ᥲძ𝗈ա  ⚡︎  𝖴𝗅𝗍𝗋𝖺۵࿐", "𝑺𝒉𝒂𝒅𝒐𝒘 𝑼𝒍𝒕𝒓𝒂 - 𝑴𝑫", "⏤͟͟͞͞☆ 𝑺𝒉𝒂𝒅𝒐𝒘 - 𝑻𝒆𝒔𝒕"]
+//Id Channel
+global.idchannel = '120363392742449171@newsletter'
+global.canalIdM = ["120363392742449171@newsletter", "120363392742449171@newsletter", "120363392742449171@newsletter"]
+global.canalNombreM = ["Pantheon Bot", "Pantheon Bot", "Pantheon Bot"]
 global.channelRD = await getRandomChannel()
-// global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "3876577197-120363302285079181@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: `${packname}`, orderTitle: 'Bang', thumbnail: icons, sellerJid: '0@s.whatsapp.net'}}}
-
-global.icono = [
-'https://files.catbox.moe/uho7vk.jpg',
-'https://files.catbox.moe/u1sgph.jpg',
-'https://files.catbox.moe/pk3xxk.jpg',
-'https://files.catbox.moe/yljhz1.jpg',
-'https://files.catbox.moe/qze4u2.jpg',
-'https://files.catbox.moe/gc6b94.jpg',
+  
+global.icono = [ 
+'https://i.ibb.co/v4Yx9Q4N/file.jpg',
+'https://i.ibb.co/v4Yx9Q4N/file.jpg',
+'https://i.ibb.co/v4Yx9Q4N/file.jpg',
+'https://i.ibb.co/v4Yx9Q4N/file.jpg',
 ].getRandom()
 
 global.urls = [
