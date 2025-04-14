aaaa9322ebea14f11e09717f5212f4b9c423c3ec
elmamonero <debastado0@gmail.com>
2025-04-14 19:26:41 -0400

    Update rpg-bank.js

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -4,7 +4,7 @@
 
 import fetch from 'node-fetch'
 import db from '../lib/database.js'
-let img = 'https://files.catbox.moe/x81ait.jpg'
+let img = 'https://files.catbox.moe/kmfqee.jpg'
 let handler = async (m, {conn, usedPrefix}) => {
    let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
    if (who == conn.user.jid) return m.react('✖️')
