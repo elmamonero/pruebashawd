1e5bebca1b52015fabf950614a03e1f336e01f44
elmamonero <debastado0@gmail.com>
2025-04-14 19:26:15 -0400

    Update buscador-githubsearch.js

--- plugins/buscador-githubsearch.js
+++ plugins/buscador-githubsearch.js
@@ -1,22 +1,28 @@
+/* Github Search By WillZek 
+- Free Codes Titan  
+- https://whatsapp.com/channel/0029ValMlRS6buMFL9d0iQ0S
+*/
+
+// [🔎] 𝗚𝗶𝘁𝗵𝘂𝗯 𝗦𝗲𝗮𝗿𝗰𝗵
+
 import fetch from 'node-fetch';
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
-if (!text) return m.reply('*🔎 Ingrese el nombre del repositorio o el del usuario.*');
+if (!text) return m.reply('《★》Ingresa Un Nombre De Repositorio o De Usuario De Github');
 
 try {
-let api = `https://dark-core-api.vercel.app/api/search/github?key=api&text=${text}`;
+let api = `https://dark-core-api.vercel.app/api/search/github?key=dk-vip&text=${text}`;
 
 let response = await fetch(api);
 let json = await response.json();
 let result = json.results[0];
 
-let txt = `\`\`\`乂 GITHUB - SEARCH\`\`\`\n\n° 🌴 *\`Nombre:\`* ${result.name}\n° 🍀 *\`Dueño:\`* ${result.creator}\n° 🌵 *\`Estrellas:\`* ${result.stars}\n° 🌿 *\`Bifurcaciones:\`* ${result.forks}\n° 🌳 *\`Descripción:\`*\n> ${result.description}\n° 🍃 *\`Creado:\`* ${result.createdAt}\n° 🪨 *\`Link:\`* ${result.cloneUrl}`;
+let txt = `*Nombre:* ${result.name}\n*Owner:* ${result.creator}\n*Estrellas:* ${result.stars}\n*Bifurcaciones:* ${result.forks}\n*Descripcion:* ${result.description}\n*Creado:* ${result.createdAt}\n*Link:* ${result.cloneUrl}`;
 
-//let img = 'https://files.catbox.moe/9vlgt5.jpg';
-//conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: fkontak });
+let img = 'https://files.catbox.moe/kmfqee.jpg';
 
-conn.sendMessage(m.chat, { text: txt }, { quoted: fkontak });
+conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: fkontak });
 
 } catch (error) {
 console.error(error)
@@ -25,6 +31,8 @@ m.react('✖️');
  }
 };
 
-handler.command = ['githubsearch', 'ghsearch'];
+handler.tag = ['buscador'];
+handler.help = ['githubsearch'];
+handler.command = ['githubsearch', 'gbsearch'];
 
 export default handler;
