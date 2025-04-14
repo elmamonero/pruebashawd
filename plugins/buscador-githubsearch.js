23769f8f87bf8f77c62d90e1bc8876ec3155dbee
elmamonero <debastado0@gmail.com>
2025-04-14 18:51:07 -0400

    Update buscador-githubsearch.js

--- plugins/buscador-githubsearch.js
+++ plugins/buscador-githubsearch.js
@@ -1,15 +1,8 @@
-/* Github Search By WillZek 
-- Free Codes Titan  
-- https://whatsapp.com/channel/0029ValMlRS6buMFL9d0iQ0S
-*/
-
-// [🔎] 𝗚𝗶𝘁𝗵𝘂𝗯 𝗦𝗲𝗮𝗿𝗰𝗵
-
 import fetch from 'node-fetch';
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
-if (!text) return m.reply('*[ 🔍 ] Ingrese el nombre del repositorio o el del usuario.');
+if (!text) return m.reply('*🔎 Ingrese el nombre del repositorio o el del usuario.*');
 
 try {
 let api = `https://dark-core-api.vercel.app/api/search/github?key=api&text=${text}`;
@@ -18,11 +11,12 @@ let response = await fetch(api);
 let json = await response.json();
 let result = json.results[0];
 
-let txt = `*Nombre:* ${result.name}\n*Owner:* ${result.creator}\n*Estrellas:* ${result.stars}\n*Bifurcaciones:* ${result.forks}\n*Descripcion:* ${result.description}\n*Creado:* ${result.createdAt}\n*Link:* ${result.cloneUrl}`;
+let txt = `\`\`\`乂 GITHUB - SEARCH\`\`\`\n\n° 🌴 *\`Nombre:\`* ${result.name}\n° 🍀 *\`Dueño:\`* ${result.creator}\n° 🌵 *\`Estrellas:\`* ${result.stars}\n° 🌿 *\`Bifurcaciones:\`* ${result.forks}\n° 🌳 *\`Descripción:\`*\n> ${result.description}\n° 🍃 *\`Creado:\`* ${result.createdAt}\n° 🪨 *\`Link:\`* ${result.cloneUrl}`;
 
-let img = 'https://files.catbox.moe/kmfqee.jpg';
+//let img = 'https://files.catbox.moe/9vlgt5.jpg';
+//conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: fkontak });
 
-conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: fkontak });
+conn.sendMessage(m.chat, { text: txt }, { quoted: fkontak });
 
 } catch (error) {
 console.error(error)
