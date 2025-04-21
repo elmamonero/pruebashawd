285b4705dbea22521e55d858a8c07e3710863c71
elmamonero <debastado0@gmail.com>
2025-04-20 22:31:10 -0400

    Update gc-encuesta.js

--- plugins/gc-encuesta.js
+++ plugins/gc-encuesta.js
@@ -1,18 +1,32 @@
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {  
+    // Verifica primero si existen argumentos  
+    if (!args[0]) {  
+        throw `⚠️️ *_Ingrese un texto para iniciar la encuesta._*\n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`;  
+    }  
 
-let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+    // Verifica que 'text' no sea undefined o null  
+    if (!text || !text.includes('|')) {   
+        throw `⚠️️ Separe las encuestas con *|*\n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`;  
+    }  
 
-if (!args[0]) throw `⚠️️ *_Ingrese un texto para iniciar la escuesta._*\n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
-if (!text.includes('|')) throw  `⚠️️ Separe las encuestas con *|* \n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
-let a = []
-let b = text.split('|')
-for (let c = 0; c < b.length; c++) {
-a.push([b[c]])
-                        }
-                        return conn.sendPoll(m.chat, `${wm}`, a, m)
-}
-handler.help = ['encuesta <text|text2>']
-handler.tags = ['grupo'] 
-handler.command = ['poll', 'encuesta'] 
-handler.group = true
+    let a = [];  
+    let b = text.split('|');  
+    
+    for (let c = 0; c < b.length; c++) {  
+        a.push([b[c]]);  
+    }  
 
-export default handler
+    // Asegúrate que 'a' tenga elementos antes de enviar la encuesta  
+    if (a.length === 0) {  
+        throw `⚠️️ No se han agregado opciones a la encuesta.`;  
+    }  
+
+    return conn.sendPoll(m.chat, `${wm}`, a, m);  
+}  
+
+handler.help = ['encuesta <text|text2>'];  
+handler.tags = ['grupo'];   
+handler.command = ['poll', 'encuesta'];   
+handler.group = true;  
+
+export default handler;  
