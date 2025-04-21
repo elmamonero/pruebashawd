a193101a6b50d928098859ce35a5ac2ccf59b892
elmamonero <debastado0@gmail.com>
2025-04-20 22:59:05 -0400

    Update freefire-versus.js

--- plugins/freefire-versus.js
+++ plugins/freefire-versus.js
@@ -1,12 +1,11 @@
 const handler = async (m, { text, conn, args, usedPrefix, command }) => {
 
     if (args.length < 2) {  
-        conn.reply(m.chat, `*[ ℹ️ ] Proporciona una hora seguido el país y una modalidad.*
-*Usa AR para Argentina y PE para Perú.*
+conn.reply(m.chat, `*[ ℹ️ ] Ingresa una hora en formato (HH:MM) seguido de AM/PM, el país y la modalidad.*
+*Usa MX para México, CO para Colombia, CL para Chile, AR para Argentina, PE para Perú y EC para Ecuador.*
 
-*[ 💡 ] Ejemplo:* .${command} 20 pe Vv2`, m);
-        return;
-    }
+*[ 💡 ] Ejemplo:* .${command} 08:30 PM PE Vv2`, m);
+
 
     // Nueva validación para formato de 24 horas
     const horaRegex = /^([01]?[0-9]|2[0-3])(:[0-5][0-9])?$/;  
